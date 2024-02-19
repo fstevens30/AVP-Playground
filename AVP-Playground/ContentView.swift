2939c77cdf027e0d844f7d2886aab9c693ad888a
Flynn Stevens <flynnstevens30@gmail.com>
2024-02-19 15:41:02 +1300

    Created app

--- AVP-Playground/ContentView.swift
+++ AVP-Playground/ContentView.swift
@@ -6,50 +6,55 @@
 //
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
-
-    @State private var showImmersiveSpace = false
-    @State private var immersiveSpaceIsShown = false
-
-    @Environment(\.openImmersiveSpace) var openImmersiveSpace
-    @Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
+    
+    @State private var catFact: String = "Press the button for a cat fact"
 
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
-
-            Toggle("Show Immersive Space", isOn: $showImmersiveSpace)
-                .toggleStyle(.button)
-                .padding(.top, 50)
+            Text("Welcome to the Cat Fact Generator.")
+                .font(.title)
+            Text(catFact)
+                .padding()
+            Button("Tell me a fact.") {
+                fetchCatFact()
+            }
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 25.0))
         }
-        .padding()
-        .onChange(of: showImmersiveSpace) { _, newValue in
-            Task {
-                if newValue {
-                    switch await openImmersiveSpace(id: "ImmersiveSpace") {
-                    case .opened:
-                        immersiveSpaceIsShown = true
-                    case .error, .userCancelled:
-                        fallthrough
-                    @unknown default:
-                        immersiveSpaceIsShown = false
-                        showImmersiveSpace = false
+    }
+    
+    func fetchCatFact() {
+        let url = URL(string: "https://meowfacts.herokuapp.com/")
+        
+        let task = URLSession.shared.dataTask(with: url!) {data, response, error in
+            if let data = data {
+                do {
+                    let response = try JSONDecoder().decode(CatFactResponse.self, from: data)
+                    DispatchQueue.main.async {
+                        if let firstFact = response.data.first {
+                            self.catFact = firstFact
+                        }
                     }
-                } else if immersiveSpaceIsShown {
-                    await dismissImmersiveSpace()
-                    immersiveSpaceIsShown = false
+                } catch {
+                    print("Failed to decode JSON: \(error)")
                 }
+            } else if let error = error {
+                print("Failed to fetch data: \(error)")
             }
         }
+        task.resume()
     }
 }
 
-#Preview(windowStyle: .automatic) {
-    ContentView()
+struct CatFactResponse: Codable {
+    let data: [String]
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
